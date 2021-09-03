6bbde5bb9414fd0ddb4daf254330adf317716776
Deepak Singh <deepakanhviet@gmail.com>
2021-09-03 14:50:18 -0400

    Init commit deepok

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,7 +6,11 @@ class Portfolio extends Component {
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
+        return (
+        <div key={projects.title} className="columns portfolio-item">
+          
+
+
            <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
                <img alt={projects.title} src={projectImage} />
@@ -19,7 +23,7 @@ class Portfolio extends Component {
               <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
-        </div>
+        </div>)
       })
     }
 

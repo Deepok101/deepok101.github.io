be7d4bc3d949bf417beddf86784571b4daeb2429
Deepak Singh <deepakanhviet@gmail.com>
2021-09-06 08:44:06 -0400

    tmp

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,26 +4,22 @@ class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
+      var projects = this.props.data.projects.map(function(project){
+        var projectImage = 'images/portfolio/'+project.image;
         return (
-        <div key={projects.title} className="columns portfolio-item">
-          
-
-
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+          <div key={project.title} className="row">
+            <div className="four columns">
+              <img alt={project.title} src={projectImage}/>
+            </div>
+            <div className="eight columns main-col">
+              <a href={project.url}><h4>{project.title}</h4></a>
+              <ul>
+                {project.description.map((desc) => <li>{desc}</li>)}
+              </ul>
+              Tech Stack: {project.stack.map((tech) => <em style={{marginLeft: "1em"}}>{tech}</em>)}
+            </div>
           </div>
-        </div>)
+        )
       })
     }
 
@@ -36,7 +32,7 @@ class Portfolio extends Component {
 
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div id="portfolio-wrapper">
                 {projects}
             </div>
           </div>

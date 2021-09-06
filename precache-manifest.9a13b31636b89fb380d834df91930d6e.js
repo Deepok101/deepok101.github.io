c95de950bab4fe9365be8a598d143361c504cab9
Deepak Singh <deepakanhviet@gmail.com>
2021-09-06 15:52:37 -0400

    Updates

---
+++ precache-manifest.9a13b31636b89fb380d834df91930d6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "ab797e1abfaa765c622e",
+    "url": "/personal-website/static/js/main.010f8808.chunk.js"
+  },
+  {
+    "revision": "2b54d9860c9bc2bd6263",
+    "url": "/personal-website/static/js/2.a2a8ea37.chunk.js"
+  },
+  {
+    "revision": "ab797e1abfaa765c622e",
+    "url": "/personal-website/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "b44f821e83eefd5b39f5982a89394ef1",
+    "url": "/personal-website/index.html"
+  }
+];
